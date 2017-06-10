33c877ea5d5bf2cb67f63fb086d77a681fc11d30
lamdav <dav_lam@ymail.com>
2017-06-10 16:18:48 -0700

    firebase autonomous color updates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,11 @@
-import { Component } from '@angular/core';
-import { AngularFireDatabase } from 'angularfire2/database';
+import {
+  Component,
+  OnInit,
+  OnDestroy
+} from '@angular/core';
+import {
+  AngularFireDatabase
+} from 'angularfire2/database';
 
 import * as firebase from 'firebase/app';
 
@@ -12,23 +18,33 @@ const VALUE = 'value';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'Favorite Things';
   favoriteColor = 'gray';
   favoriteNumber = 0;
   firebaseDB = firebase.database().ref(FAVORITE_THINGS_PATH).child(COLOR_PATH);
 
   constructor(db: AngularFireDatabase) {
+
   }
+
+  ngOnInit(): void {
+    this.firebaseDB.on(VALUE, (snapshot: firebase.database.DataSnapshot) => {
+      this.favoriteColor = snapshot.val();
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.firebaseDB.off();
+  }
+
 
   setColor(selectedColor: string): void {
     this.firebaseDB.set(selectedColor);
   }
 
   updateColor(): void {
-    this.firebaseDB.once(VALUE, (snapshot: firebase.database.DataSnapshot) => {
-      this.favoriteColor = snapshot.val();
-    });
+
   }
 
   setNumber(number: number): void {
